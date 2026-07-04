67c0d99d4aa74e36bbe4fe0be547d07ccf74f5df
mac <@b.tunde.ipaye@gmail.com>
2026-07-04 17:38:15 +0100

    feat: auth class updated commit by ENGR. IPAYE

---
+++ src/main/java/dev/engripaye/software/auth/LoginRequest.java
@@ -0,0 +1,14 @@
+package dev.engripaye.software.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
