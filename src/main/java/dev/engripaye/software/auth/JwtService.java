db15a5e9905514cc3057bda547c4981ca05a2467
mac <@b.tunde.ipaye@gmail.com>
2026-07-04 17:49:24 +0100

    feat: auth class updated commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/software/auth/JwtService.java
+++ src/main/java/dev/engripaye/software/auth/JwtService.java
@@ -1,13 +1,27 @@
 package dev.engripaye.software.auth;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = "this-is-a-very-long-secret-key-for-projecthub-authentication";
 
-    private static final long EXPIRATION = 1000 * 60 * 60 * 24;
+    private static final long EXPIRATION = 1000 * 60 * 60 * 24; // 24hours
 
-    
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+
+
+
+
+
 }
